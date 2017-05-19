41cb8a670435c9d92e249c751ac421589ddc6b93
Vladyslav Tychyna <tychyna.te@gmail.com>
2017-05-19 10:56:48 +0300

    fix bugs

--- views/request/index.php
+++ views/request/index.php
@@ -8,7 +8,7 @@
 echo \app\widgets\Alert::widget();
 ?>
 <div class="content-header">
-    <img src="../images/feedback-logo.jpg" class="logo" alt="logo">
+    <img src="./images/feedback-logo.jpg" class="logo" alt="logo">
     <h1 class="title">Подать заявку</h1>
     <p class="description">Все поля обязательны для заполнения. Заявка приходит на имейл.</p>
 </div>
